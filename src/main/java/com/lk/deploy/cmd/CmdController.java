1d255a12bbbcf4be4c0cb51b7a3f5bdc1396568a
xiaokuan.ren <renxiaokuan9428@duia.com>
2020-06-02 20:12:30 +0800

    修改参数

--- src/main/java/com/lk/deploy/cmd/CmdController.java
+++ src/main/java/com/lk/deploy/cmd/CmdController.java
@@ -22,7 +22,7 @@ public class CmdController {
         if (StringUtils.isEmpty(dir)) {
             return ResponseEntity.badRequest().body("缺少参数");
         }
-        String file_path = "/home/travis/repository"+dir;
+        String file_path = "/home/travis/repository/"+dir;
         File file = new File(file_path);
         String[] cmds = new String[]{"sh","./build.sh"};
         try {
